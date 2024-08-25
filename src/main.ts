f67f75865bca07748b6073dc99e34d70c9a90904
razl92@163.com <8791173qq>
2024-08-25 17:57:06 +0800

    ✨特性(custom): 添加 封装 git SvgIcon 通用组件

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+
+import installIcons from '@/icons/index'
+import 'virtual:svg-icons-register'
+
+import 'element-plus/dist/index.css'
+import './styles/index.less'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +15,8 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus, { size: 'small', zIndex: 3000 })
+
+installIcons(app)
 
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,34 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import eslint from 'vite-plugin-eslint'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools(), eslint()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    eslint(),
+    createSvgIconsPlugin({
+      // 指定要缓存的图标目录
+      iconDirs: [path.resolve(process.cwd(), 'src/icons/svg')],
+      // 指定symbolId格式
+      symbolId: 'icon-[dir]-[name]',
+
+      /**
+       * 自定义插入位置
+       * 默认值: body-last
+       */
+      inject: 'body-last',
+      /**
+       * 自定义DOM id
+       * 默认值: __svg__icons__dom__
+       */
+      customDomId: '__svg__icons__dom__'
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
